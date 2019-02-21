6c178b81b520d2b3787eeb77c496ae9eb72a7aaf
KarolinaH <khajzer@oakfusion.pl>
2019-02-21 19:10:13 +0100

    adding homepage - list of conferences

--- goconf/conferences/views.py
+++ goconf/conferences/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Conf
 
-# Create your views here.
+
+def conf_list(request):
+    confs = Conf.objects.filter(published_date__lte=timezone.now()).order_by('published_post_date')
+    return render(request, '~/goconf/goconf/templates/pages/home.html', {'confs': confs})
