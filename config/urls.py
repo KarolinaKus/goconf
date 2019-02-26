bb7efc5b5e235ad67627029aa231d41d91d39a69
KarolinaH <khajzer@oakfusion.pl>
2019-02-26 18:45:54 +0100

    working homepage

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from goconf.conferences.views import conf_list
+
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", view=conf_list, name="home"),
     path(
         "about/",
         TemplateView.as_view(template_name="pages/about.html"),
--- goconf/conferences/views.py
+++ goconf/conferences/views.py
@@ -4,5 +4,5 @@ from .models import Conf
 
 
 def conf_list(request):
-    confs = Conf.objects.filter(published_date__lte=timezone.now()).order_by('published_post_date')
-    return render(request, '~/goconf/goconf/templates/pages/home.html', {'confs': confs})
+    confs = Conf.objects.filter(published_post_date__lte=timezone.now()).order_by('published_post_date')
+    return render(request, 'pages/home.html', {'confs': confs})
