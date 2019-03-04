09ee8266f1485f09dd9098827c0d66678fd107ca
KarolinaH <khajzer@oakfusion.pl>
2019-03-04 14:45:45 +0100

    removed publish

--- goconf/conferences/models.py
+++ goconf/conferences/models.py
@@ -8,12 +8,7 @@ class Conference(models.Model):
     description = models.TextField()
     participants_number = models.IntegerField()
     place = models.CharField(max_length=100)
-    published_post_date = models.DateTimeField(blank=True, null=True)
     title = models.CharField(max_length=200)
-
-    def publish(self):
-        self.published_post_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.title
--- goconf/conferences/views.py
+++ goconf/conferences/views.py
@@ -4,5 +4,5 @@ from .models import Conference
 
 
 def conf_list(request):
-    confs = Conference.objects.filter(published_post_date__lte=timezone.now()).order_by('published_post_date')
+    confs = Conference.objects.order_by('date')
     return render(request, 'pages/home.html', {'confs': confs})
